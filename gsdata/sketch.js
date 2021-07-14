d78c2b85e953d6560fb9a38301d54036a4ad324e
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-07-14 21:20:20 +0530

    Update sketch.js

--- gsdata/sketch.js
+++ gsdata/sketch.js
@@ -1,13 +1,16 @@
 var data ;
 function setup() {
   createCanvas(400, 400);
+  background(220);
   Tabletop.init( { key: "1ZCMFzxa0QVDryxzf4AiPTv9NsD1NDxdrHy0Kbs77aA8",
                      callback: showInfo,
                      simpleSheet: true } )
 }
 
 function showInfo(data,tabletop){
-  
+  for(var i=0;i<data.length;i++){
+    text(data[i].Name,10,30+i*10);
+  }
   console.log(data);
 }
 
